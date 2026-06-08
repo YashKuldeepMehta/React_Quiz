d641e04e7ded193bccc2b3c74a7f7942881ea093
Yash Mehta <113234432+YashKuldeepMehta@users.noreply.github.com>
2026-06-08 16:43:56 +0530

    Fix formatting of score display in ScoreCard component

--- quiz/src/components/ScoreCard..jsx
+++ quiz/src/components/ScoreCard..jsx
@@ -4,7 +4,7 @@ const ScoreCard = ({score,total})=>{
     return (
         <>
         <h2>Quiz Completed!!</h2>
-        <h3>Your score : <b>{score} / {total}</b></h3>
+        <h3>Your score : <b>{score} / {total}</b>
         </>
     )
 }
